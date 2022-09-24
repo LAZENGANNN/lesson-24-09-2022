8c6c339ce7737211df3f3493155a704879832372
LAZENGANNN <113790875+LAZENGANNN@users.noreply.github.com>
2022-09-24 19:17:12 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,154 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+
+	//Boolean1
+	//int a;
+	//bool res;
+	//cin >> a;
+	//res = a > 0;
+	//cout << boolalpha << res;
+
+	//Boolean2◦
+	//int a;
+	//bool res;
+	//cin >> a;
+	//res = a % 2 != 0;
+	//cout << boolalpha << res;
+
+	//Boolean3◦
+	/*int a;
+	bool res;
+	cin >> a;
+	res = a % 2 == 0;
+	cout << boolalpha << res;*/
+
+	//Boolean4◦
+	/*int a;
+	int b;
+	bool res;
+	cin >> a >> b;
+	res = a > 2 &&  b <= 3;
+	cout << boolalpha << res;
+	*/
+
+	//Boolean5◦
+	/*int a;
+	int b;
+	bool res;
+	cin >> a >> b;
+	res = a >= 0 || b < -2;
+	cout << boolalpha << res;*/
+
+	//Boolean6◦
+	/*int a;
+	int b;
+	int c;
+	bool res;
+	cin >> a >> b >> c;
+	res = a < b && b < c;
+	cout << boolalpha << res;*/
+
+	//Boolean7◦
+	/*int a;
+	int b;
+	int c;
+	bool res;
+	res = (a < b && b < c) || (a > b && b > c) ;
+	cout << boolalpha << res;*/
+
+	//Boolean8◦
+	/*int a;
+	int b;
+	bool res;
+	cin >> a >> b;
+	res = (a % 2 != 0) && (b % 2 != 0);
+	cout << boolalpha << res;*/
+
+	//Boolean9◦
+	/*int a;
+	int b;
+	bool res;
+	cin >> a >> b;
+	res = (a % 2 != 0) || (b % 2 != 0);
+	cout << boolalpha << res;*/
+
+	//Boolean10◦
+	/*int a;
+	int b;
+	bool res;
+	cin >> a >> b;
+	res = ((a % 2 != 0) & (b % 2 == 0)) || ((a % 2 == 0) & (b % 2 != 0));
+	cout << boolalpha << res;*/
+
+	//Boolean11◦
+	/*int a;
+	int b;
+	bool res;
+	cin >> a >> b;
+	res = ((a % 2 != 0) && (b % 2 != 0)) || ((a % 2 == 0) && (b % 2 == 0));
+	cout << boolalpha << res;*/
+
+	//Boolean12◦
+	/*int a;
+	int b;
+	int c;
+	bool res;
+	cin >> a >> b >> c;
+	res = (a > 0) && (b > 0) && (c > 0);
+	cout << boolalpha << res;*/
+
+	//Boolean13◦
+	/*int a;
+	int b;
+	int c;
+	bool res;
+	cin >> a >> b >> c;
+	res = (a > 0) || (b > 0) || (c > 0);
+	cout << boolalpha << res;
+	*/
+
+//Boolean14◦
+//int a;
+//int b;
+//int c;
+//bool res;
+//cin >> a >> b >> c;
+//res = ((a > 0) & (b < 0) & (c < 0)) || ((a < 0) & (b > 0) & (c < 0)) || ((a < 0) & (b < 0) & (c > 0));
+//cout << boolalpha << res;
+
+//Boolean15◦
+//int a;
+//int b;
+//int c;
+//bool res;
+//cin >> a >> b >> c;
+//res = ((a > 0) & (b > 0) & (c < 0)) || ((a < 0) & (b > 0) & (c > 0)) || ((a > 0) & (b < 0) & (c > 0));
+//cout << boolalpha << res;
+
+//Boolean16◦
+//int a;
+//bool res;
+//cin >> a;
+//res = a > 9 & a < 100 & (a % 2) == 0;
+//cout << boolalpha << res;
+
+//Boolean17◦
+//int a;
+//bool res;
+//cin >> a;
+//res = a > 99 & a < 1000 & (a % 2) == 0;
+//cout << boolalpha << res;
+
+
+
+	
+
+
+
+
+
+
+
+	
+}
